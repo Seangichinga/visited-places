a5e0d7dad4e2d3100ddde7cb35d2ab04aa8d58ad
Sean Gathecere Gichinga <seangichinga@gmail.com>
2026-02-15 14:01:27 +0300

    form collector and new object

--- script.js
+++ script.js
@@ -5,4 +5,18 @@ const placeNameInput = document.getElementById('placeName');
 const placeDescriptionInput = document.getElementById('placeDescription');
 const timeOfYearInput = document.getElementById('timeOfYear');
 const placesList = document.getElementById('placesList');
-const totalPlacesSpan = document.getElementById('totalPlaces');
+
+addPlaceForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = placeNameInput.value.trim();
+    const description = placeDescriptionInput.value.trim();
+    const timeOfYear = timeOfYearInput.value;
+});
+
+const newPlace = new Place(
+        name,          
+        [],             
+        timeOfYear,     
+        description,    
+        new Date(),                  
+    );
