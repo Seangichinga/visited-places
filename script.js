61dc19f295e82e94fa864d4aac2f08462d54985b
Sean Gathecere Gichinga <seangichinga@gmail.com>
2026-02-15 13:54:18 +0300

    Intial JS additions

---
+++ script.js
@@ -0,0 +1,8 @@
+const travelLog = new TravelLog();
+
+const addPlaceForm = document.getElementById('addPlaceForm');
+const placeNameInput = document.getElementById('placeName');
+const placeDescriptionInput = document.getElementById('placeDescription');
+const timeOfYearInput = document.getElementById('timeOfYear');
+const placesList = document.getElementById('placesList');
+const totalPlacesSpan = document.getElementById('totalPlaces');
